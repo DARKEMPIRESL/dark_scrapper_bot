7015dd5b2bf36665ceef3178aa2a12ad1db0500b
DARKEMPIRE <95665347+DARKEMPIRESL@users.noreply.github.com>
2022-05-30 09:12:10 +0530

    dark_scrapper_bot

---
+++ translation.py
@@ -0,0 +1,38 @@
+class Translation(object):
+    START_TEXT = """<b>𝗗𝗔𝗥𝗞 𝗧𝗚 𝗦𝗖𝗥𝗔𝗣𝗣𝗘𝗥 𝗕𝗢𝗧 is made to Help you to Retrieve APP ID and API Hash easily and SAFELY!
+━━━━━━━━━━━━━━━━━━━━━━━━
+Please Enter Your Telegram Phone Number With Country Code Format.
+Example : +947xxxxxxx</b>
+
+Dev - @ImDark_Empire | Official Channel - @SLBotOfficial
+"""
+    AFTER_RECVD_CODE_TEXT = """HP No Accepted!
+Please send us the code you received from Telegram!
+
+This code is only used for the purpose of getting APP ID from my.telegram.org
+if you don't trust this dev bot, just take it manually
+
+Dev - @ImDark_Empire | Official Channel - @SLBotOfficial
+
+
+"""
+    BEFORE_SUCC_LOGIN = "Code Received. Scarpping Web Page..."
+    ERRED_PAGE = """Error. Try the Manual Way
+
+How to Download APP ID and API HASH Manually
+1. Go to my.telegram.org/auth
+2. Login your telegram account
+3. Click the API Development menu
+4. Fill in the data as below :
+• App Title : tgbot
+• Short Name : tgbot
+• URL : (empty)
+• Platform : desktop
+5. Done!
+
+If You Successfully Download Manual Please Try Again on this Bot
+
+Dev - @ImDark_Empire | Official Channel - @SLBotOfficial"""
+    CANCELLED_MESG = "Bye! Please /start to repeat"
+    IN_VALID_CODE_PVDED = "The OTP code you entered is WRONG"
+    IN_VALID_PHNO_PVDED = "The mobile number you entered is WRONG, please enter your telegram phone number with the country code format.\Example: +947xxxxxxx"
